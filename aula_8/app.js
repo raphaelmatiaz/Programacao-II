e373004aa9c752417536da9e319f2b69fbf18987
Rafael Matias <rafael_matias@eticalgarve.com>
2024-01-25 21:47:06 +0000

    Aula 9

--- aula_8/app.js
+++ aula_8/app.js
@@ -1,21 +1,3 @@
-window.onload = () => {                                         
-
-    const large = document.querySelector(".large");
-    const small = document.querySelector(".small");
-
-    large.onclick = () => {
-        small.dispatchEvent(new CustomEvent('small_event'));
-
-    }
-
-    small.addEventListener("small_event", () => {
-        console.log("large clicked");
-    });
-}
-
-
-
-
 
 
 
---
+++ aula_9/app.js
@@ -0,0 +1,51 @@
+//------------------------------------------
+// WINDOW ONLOAD
+
+console.log("1")
+
+window.onload = () => {
+    console.log("window loaded 1")
+}
+
+console.log("2")
+
+//-------------------------------------------
+// ADICIONAR a e b
+
+let numero1 = 40;
+let numero2 = 60
+
+
+const adicionar = (a, b) => {
+    result = a + b;
+    console.log(result)
+}
+
+adicionar(numero1, numero2)
+
+
+
+//--------------------------------------------
+
+// MULTIPLICAR POR c por d
+
+const multiplicar = (c, d) => {
+    result = c * d
+    console.log(result);
+}
+
+multiplicar(numero1, numero2);
+
+
+//---------------------------------------------
+
+// MULTIPLICAR O RESULTADO DA PRIMEIRA FUNCAO POR 2
+
+
+//---------------------------------------------
+
+const arr = [2, 45, 34, 2];
+
+array.forEach(element => {
+    
+});
