87d3cefb819d5f24fce1d547d1abf66536ff3b30
Rafael Matias <rafael_matias@eticalgarve.com>
2024-01-31 21:31:58 +0000

    Aula 12

---
+++ aula12/AudioPlayer.js
@@ -0,0 +1,26 @@
+export default class AudioPlayer {
+
+    #audio;
+    constructor() {
+        this.#audio = document.querySelector("audio");
+
+        const button = document.querySelector("button");
+        button.onclick = () => {
+            this.#stop();
+        }
+    }
+
+    play(src) {
+
+        this.#audio.src = src;
+        this.#audio.play();
+
+    }
+
+    #stop() {
+        console.log("stop")
+        this.#audio.pause();
+    }
+}
+
+
