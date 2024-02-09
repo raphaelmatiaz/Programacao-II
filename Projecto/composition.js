6aa3ae65640315bd0bd102e97f1d80f141ec14f1
Rafael Matias <rafamatias10@gmail.com>
2024-02-09 23:22:51 +0000

    Projecto Final: Project Mostly Finished

--- Projecto/composition.js
+++ Projecto/composition.js
@@ -7,9 +7,9 @@ export default class Composition extends ArtPiece {
 
         this.audio = data.audio;
         
-        let playStatus = null;
-        let audioPlayer;
-        let currentSong;
+        // let playStatus = null;
+        // let audioPlayer;
+        // let currentSong;
 
 
         const title = document.getElementById("artworks-title");
@@ -22,39 +22,72 @@ export default class Composition extends ArtPiece {
         title.textContent = "Compositions";
         })
 
+
+        // this.view.addEventListener('click', () => {
+                
+        //     switch (playStatus) {
+
+        //             case null:
+
+        //                 if (currentSong != this.name) {
+        //                     const allAudios = document.querySelectorAll('audio')
+        //                     allAudios.forEach(audio => {
+        //                         audio.parentNode.removeChild(audio);
+        //                     });
+        //                 } 
+                        
+        //                 else {
+        //                 audioPlayer = document.createElement("audio");
+        //                 audioPlayer.src = this.audio;
+        //                 this.view.appendChild(audioPlayer);
+        //                 audioPlayer.play();
+        //                 playStatus = "playing";
+        //                 currentSong = this.name;
+        //                 }
+        //                 break;
+
+        //             case "playing":
+        //                 // Pause the audio
+        //                 audioPlayer.pause();
+        //                 // Update the play status
+        //                 playStatus = "paused";
+        //                 break;
+        //             case "paused":
+        //                 // Resume playing the audio
+        //                 audioPlayer.play();
+        //                 // Update the play status
+        //                 playStatus = "playing";
+        //                 break;
+        //         }
+        //     });
+
+        let song = document.querySelector('audio');   
+        let currentSong;
+        let isPlaying = false;
+
         this.view.addEventListener('click', () => {
-            let audioPlayer = this.view.querySelector('audio'); // Check if an audio player already exists
-                switch (playStatus) {
-                    case null:
+            currentSong = this.name;
+            song.src = this.audio;
 
-
-                        audioPlayer = document.createElement("audio");
-                        // Set the audio source
-                        audioPlayer.src = this.audio;
-                        // Append the audio element to the view
-                        this.view.appendChild(audioPlayer);
-                        // Play the audio
-                        audioPlayer.play();
-                        // Update the play status
-                        playStatus = "playing";
-                        break;
-                    case "playing":
-                        // Pause the audio
-                        audioPlayer.pause();
-                        // Update the play status
-                        playStatus = "paused";
-                        break;
-                    case "paused":
-                        // Resume playing the audio
-                        audioPlayer.play();
-                        // Update the play status
-                        playStatus = "playing";
-                        break;
+            if (currentSong === this.name) {
+                if (isPlaying) {
+                    song.pause();
+                    isPlaying = false;
+                } else {
+                    song.play();
+                    isPlaying = true;
                 }
-            });
-            
+            } else {
+                // Stop the current song and play the new one
+                song.pause();
+                song.currentTime = 0; // Reset the playback position
+                song.src = this.audio;
+                song.play();
+                isPlaying = true;
+                currentSong = this.name;
+            }
+        });
         };
-        
     }
 
     
